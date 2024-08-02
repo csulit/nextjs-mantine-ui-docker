b2b336b3c761d22b9cb593a2f212f00535c36d3a
casulit <chrisgelosulit@gmail.com>
2024-08-02 13:24:58 +0800

    chore: Rename schema file and update config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'drizzle-kit';
 export default defineConfig({
   dialect: 'postgresql',
   out: 'drizzle/migrations',
-  schema: 'database/schema',
+  schema: 'database/schema.ts',
   verbose: true,
   strict: true,
 });
