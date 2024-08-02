99d2b70bdb7b79fbad66d5c1e24e609ea8c94d66
casulit <chrisgelosulit@gmail.com>
2024-08-02 13:38:51 +0800

    refactor: Consolidate live chat into component

--- app/page.tsx
+++ app/page.tsx
@@ -1,51 +1,13 @@
-'use client';
-
-import { useState } from 'react';
-import { ActionIcon, Affix, Flex } from '@mantine/core';
-import { useMediaQuery, useViewportSize } from '@mantine/hooks';
-import { IconMessage } from '@tabler/icons-react';
-import Iframe from 'react-iframe';
 import { Welcome } from '../components/Welcome/Welcome';
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
-import { env } from '@/utils/env';
+import PublicLiveChat from '@/components/PublicLiveChat/PublicLiveChat';
 
 export default function HomePage() {
-  const [open, setOpen] = useState(false);
-  const { height, width } = useViewportSize();
-  const isBigScreen = useMediaQuery('(min-width: 640px)');
-
   return (
     <>
       <Welcome />
       <ColorSchemeToggle />
-      <Affix position={{ bottom: 10, right: 10 }}>
-        <Flex direction="column" justify="flex-end" align="flex-end" gap={10}>
-          <Iframe
-            scrolling="no"
-            loading="eager"
-            allowFullScreen
-            id="kmc-live-chat-pop-up"
-            url={`${env.NEXT_PUBLIC_APP_URL}/public/livechat`}
-            width={!isBigScreen ? `${width - 20}px` : '400px'}
-            height={!isBigScreen ? `${height - 100}px` : `${height - height * 0.3}px`}
-            styles={{
-              border: 'none',
-              borderRadius: 16,
-              display: open ? 'block' : 'none',
-              boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-            }}
-          />
-          <ActionIcon
-            size="xl"
-            radius={100}
-            loading={false}
-            loaderProps={{ type: 'dots' }}
-            onClick={() => setOpen(!open)}
-          >
-            <IconMessage />
-          </ActionIcon>
-        </Flex>
-      </Affix>
+      <PublicLiveChat />
     </>
   );
 }
--- components/LiveChatIframe/LiveChatIframe.tsx
+++ components/LiveChatIframe/LiveChatIframe.tsx
@@ -7,9 +7,15 @@ interface LiveChatIframeProps {
 }
 
 export function LiveChatIframe({ open }: LiveChatIframeProps) {
-  const { height: viewportHeight, width: viewportWidth } = useViewportSize();
   const colorScheme = useColorScheme();
   const isBigScreen = useMediaQuery('(min-width: 640px)');
+  const { height: viewportHeight, width: viewportWidth } = useViewportSize();
+
+  const getIframeWidth = () => (!isBigScreen ? `${viewportWidth - 20}px` : '400px');
+  const getIframeHeight = () =>
+    !isBigScreen ? `${viewportHeight - 100}px` : `${viewportHeight - viewportHeight * 0.3}px`;
+  const getBoxShadow = () =>
+    `0 4px 8px ${colorScheme === 'light' ? 'rgba(0, 0, 0, 0.1)' : 'rgba(0, 0, 0, 0.5)'}`;
 
   return (
     <Iframe
@@ -18,13 +24,13 @@ export function LiveChatIframe({ open }: LiveChatIframeProps) {
       allowFullScreen
       id="kmc-live-chat-pop-up"
       url={`${env.NEXT_PUBLIC_APP_URL}/public/livechat`}
-      width={!isBigScreen ? `${viewportWidth}px` : '400px'}
-      height={!isBigScreen ? `${viewportHeight - 5}px` : '560px'}
+      width={getIframeWidth()}
+      height={getIframeHeight()}
       styles={{
         display: open ? 'block' : 'none',
         border: 'none',
         borderRadius: 16,
-        boxShadow: `0 4px 8px ${colorScheme === 'light' ? 'rgba(0, 0, 0, 0.1)' : 'rgba(0, 0, 0, 0.5)'}`,
+        boxShadow: getBoxShadow(),
       }}
     />
   );
